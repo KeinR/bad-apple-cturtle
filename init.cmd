b779dedd5c484d169417636e2b458da76b39dcea
Orion Musselman <musselmano@berea.edu>
2021-08-30 00:08:51 -0400

    Update script

--- init.cmd
+++ init.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 mkdir frames
-youtube-dl "https://www.youtube.com/watch?v=FtutLA63Cp8" -o video.mp4
-ffmpeg -i video.mp4 frames/%%04d.png -hide_banner
+youtube-dl "https://www.youtube.com/watch?v=FtutLA63Cp8" -o video
+ffmpeg -i video.mkv frames/%%04d.png -hide_banner
 pause
 
